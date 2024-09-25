3a4f2312521c39e31a9c05c95d4bf35b6862469c
Ezequiel <ezequiel.carrizo.ac+2@gmail.com>
2024-09-26 00:51:14 +0100

    test: Test setup, adding global jest configuration.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,23 @@
 import 'reflect-metadata'
+
+// Create a mock for the Resource class
+jest.mock('sst', () => {
+  return {
+    Resource: {
+      OpenaiApiKey: {
+        value: 'mocked-api-key',
+      },
+    },
+  }
+})
+
+// New Date() always return the same date.
+const mockDate = new Date(2024, 0, 1)
+export const mockedIsoStringDate = mockDate.toISOString()
+jest.spyOn(global, 'Date').mockImplementation(() => mockDate)
+
+// V4() always return the same UUID.
+export const mockedUuid = '00000000-0000-0000-0000-000000000000'
+jest.mock('uuid', () => ({
+  v4: () => mockedUuid,
+}))
