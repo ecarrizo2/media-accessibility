b0c991766e57f4da8443be668a11cd922e1c7213
Ezequiel <ezequiel.carrizo.ac+2@gmail.com>
2024-09-26 00:54:27 +0100

    test(JobCommandHandlers): Added Create Job Command Handler tests.

---
+++ src/application/command-handlers/job/create-job.command-handler.spec.ts
@@ -0,0 +1,39 @@
+import { CreateJobCommandHandler } from '@application/command-handlers/job/create-job.command-handler'
+import { createMock } from '@golevelup/ts-jest'
+import { JobRepository } from '@domain/repositories/job/job-repository.interface'
+import { CreateJobCommand } from '@application/commands/job/create-job.command'
+import { JobStatus, JobType } from '@domain/enums/job/job.enum'
+import { JobEntity } from '@domain/entities/job/job.entity'
+
+describe('CompleteJobCommandHandler', () => {
+  let commandHandler: CreateJobCommandHandler
+  const jobRepository = createMock<JobRepository>()
+
+  beforeEach(() => {
+    commandHandler = new CreateJobCommandHandler(jobRepository)
+  })
+
+  describe('WHEN handling the command', () => {
+    it('THEN it should complete the job and save it', async () => {
+      const command = CreateJobCommand.from({
+        type: JobType.ImageProcessing,
+        input: { url: 'http://localhost:8080', prompt: 'string' },
+      })
+
+      const unitResult = await commandHandler.handle(command)
+
+      expect(jobRepository.save).toHaveBeenCalledTimes(1)
+      expect(jobRepository.save).toHaveBeenCalledWith(expect.any(JobEntity))
+      expect(unitResult).toBeInstanceOf(JobEntity)
+      expect(unitResult).toEqual(
+        expect.objectContaining({
+          type: command.type,
+          status: JobStatus.Pending,
+          input: command.input,
+          attempts: 0,
+          errors: null,
+        })
+      )
+    })
+  })
+})
