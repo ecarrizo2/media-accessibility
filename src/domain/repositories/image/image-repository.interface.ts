e047706ddcc88f5d07047e2549b1f5b2af26c742
Ezequiel <ezequiel.carrizo.ac+2@gmail.com>
2024-09-30 20:08:51 +0100

    chore(actions): Removed redundant comments.

--- src/domain/repositories/image/image-repository.interface.ts
+++ src/domain/repositories/image/image-repository.interface.ts
@@ -1,10 +1,6 @@
 import { ImageEntity } from '../../entities/image/image.entity'
 import { Repository } from '@domain/repositories/repository.interface'
 
-/**
- * Interface representing a repository for managing Image entities.
- * Extends the BaseRepository interface to provide specific implementations for ImageEntity.
- */
 export interface ImageRepository extends Repository<ImageEntity> {
   /**
    * Finds an image by its URL.
