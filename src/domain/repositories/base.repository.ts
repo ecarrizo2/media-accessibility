27cd5d0f4b09d846a00eca6972b2d1b8bceee029
Ezequiel <ezequiel.carrizo.ac+2@gmail.com>
2024-10-01 18:55:38 +0100

    chore: removed no-need comments..

--- src/domain/repositories/base.repository.ts
+++ src/domain/repositories/base.repository.ts
@@ -3,13 +3,6 @@ import { ClassConstructor, instanceToPlain, plainToInstance } from 'class-transf
 export abstract class EntityTransformableRepository<EntityType> {
   protected entityClass: ClassConstructor<EntityType> | undefined
 
-  /**
-   * Converts a DynamoDB item to an entity.
-   * Must be implemented in the derived class.
-   *
-   * @param {Record<string, unknown>} item - The DynamoDB item.
-   * @returns {unknown} - The entity.
-   */
   protected toEntity(item: Record<string, unknown>): EntityType {
     if (!this.entityClass) {
       throw new Error('Abstract implementation is wrong, Entity class not defined')
@@ -18,13 +11,6 @@ export abstract class EntityTransformableRepository<EntityType> {
     return plainToInstance(this.entityClass, item)
   }
 
-  /**
-   * Converts an entity to a DynamoDB item.
-   * Must be implemented in the derived class.
-   *
-   * @param {unknown} entity - The entity.
-   * @returns {any} - The DynamoDB item.
-   */
   protected toItem(entity: EntityType): Record<string, unknown> {
     return instanceToPlain(entity)
   }
