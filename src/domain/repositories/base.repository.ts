773d07696fad97665df3e8e37af355520c81246c
Ezequiel <ezequiel.carrizo.ac+2@gmail.com>
2024-10-01 18:39:47 +0100

    chore: fixed JS Docs and removed commented code.

--- src/domain/repositories/base.repository.ts
+++ src/domain/repositories/base.repository.ts
@@ -7,8 +7,8 @@ export abstract class EntityTransformableRepository<EntityType> {
    * Converts a DynamoDB item to an entity.
    * Must be implemented in the derived class.
    *
-   * @param {any} item - The DynamoDB item.
-   * @returns {EntityType} - The entity.
+   * @param {Record<string, unknown>} item - The DynamoDB item.
+   * @returns {unknown} - The entity.
    */
   protected toEntity(item: Record<string, unknown>): EntityType {
     if (!this.entityClass) {
@@ -22,18 +22,10 @@ export abstract class EntityTransformableRepository<EntityType> {
    * Converts an entity to a DynamoDB item.
    * Must be implemented in the derived class.
    *
-   * @param {EntityType} entity - The entity.
+   * @param {unknown} entity - The entity.
    * @returns {any} - The DynamoDB item.
    */
   protected toItem(entity: EntityType): Record<string, unknown> {
     return instanceToPlain(entity)
   }
-
-  // findById(id: string): Promise<EntityType | null> {
-  //   return Promise.resolve(undefined)
-  // }
-  //
-  // save(entity: EntityType): Promise<void> {
-  //   return Promise.resolve(undefined)
-  // }
 }
